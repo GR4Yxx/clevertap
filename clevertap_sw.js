219e4681358e984c442a43a80e00a1944b210213
Gray X <layfonalseif10@gmail.com>
2024-12-04 19:39:32 -0700

    added more things

--- clevertap_sw.js
+++ clevertap_sw.js
@@ -1,17 +1,3 @@
-// Log when service worker is installed
-self.addEventListener("install", function (event) {
-  console.log("Service Worker installed");
-});
-
-// Log when service worker is activated
-self.addEventListener("activate", function (event) {
-  console.log("Service Worker activated");
-});
-
-// Handle push events
-self.addEventListener("push", function (event) {
-  console.log("Push received:", event);
-});
-
-// Import CleverTap service worker script
-importScripts("https://d2r1yp2w7bby2u.cloudfront.net/js/clevertap_sw.js");
+importScripts(
+  "https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js"
+);

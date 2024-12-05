412314f2c038eccb3d885f6381a6c2484999e500
Gray X <layfonalseif10@gmail.com>
2024-12-04 19:54:48 -0700

    asd

--- clevertap_sw.js
+++ clevertap_sw.js
@@ -1,3 +1,19 @@
 importScripts(
   "https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js"
 );
+
+self.addEventListener("install", function (event) {
+  console.log("Service Worker installing.");
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("Service Worker activating.");
+});
+
+self.addEventListener("push", function (event) {
+  console.log("Push event received:", event);
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("Notification click received:", event);
+});
